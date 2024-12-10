db449c56ffddbe7c6a73737eadaab442d42bc6f8
barry <barry@barry.myguest.virtualbox.org>
2024-12-10 11:19:27 +0800

    To implement '+' and '-' operator and add a test for operator implementation

--- main.c
+++ main.c
@@ -6,10 +6,29 @@ int main(int argc, char **argv){
     fprintf(stderr, "%s: no argument is specified, exit.\n", argv[0]);
     return 1;
   }
+  
+  char *p = argv[1];
 
   printf("  .global main\n");
   printf("main:\n");
-  printf("  mov $%d, %%eax\n", atoi(argv[1]));
+  printf("  mov $%ld, %%eax\n", strtol(p, &p, 10));
+
+  while(*p) {
+    if(*p == '+') {
+    	p++;
+	printf("  add $%ld, %%eax\n", strtol(p, &p, 10));
+        continue;
+    }
+
+    if(*p == '-') {
+        p++;
+        printf("  sub $%ld, %%eax\n", strtol(p, &p, 10));
+	continue;
+    }
+    
+    fprintf(stderr, "unexpected character: '%c'\n", *p);
+  }
+
   printf("  ret\n");
   return 0;
 }
